86f6f836e289ff89b068bc1085073b1726965d3f
Azzazy6269 <mohamadelazzazy@gmail.com>
2026-06-29 05:52:06 +0300

    feat(server)/implement websocket actions

--- server/index.js
+++ server/index.js
@@ -3,19 +3,18 @@ const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
+const chatRoutes = require('./routes/chatRoutes');
+const messageRoutes = require('./routes/messageRoutes');
 
 dotenv.config();
+connectDB();
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-const connectDB = require('./config/db');
-connectDB();
-
-const userRoutes = require('./routes/userRoutes');
-const chatRoutes = require('./routes/chatRoutes');
-const messageRoutes = require('./routes/messageRoutes');
 app.use('/api/users', userRoutes);
 app.use('/api/chats', chatRoutes);
 app.use('/api/messages', messageRoutes);
@@ -23,19 +22,50 @@ app.use('/api/messages', messageRoutes);
 const server = http.createServer(app);
 
 const io = new Server(server, {
+  pingTimeout: 60000, 
   cors: {
-    origin: process.env.FRONTEND_URL,
-    methods: ["GET", "POST"]
+    origin: "http://localhost:3000",
   }
 });
 
-io.on('connection', (socket) => {
-  console.log(`User connected: ${socket.id}`);
+// WebSockets (Socket.io)
 
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
+io.on('connection', (socket) => {
+  console.log('Connected to socket.io successfully ✅');
+
+  socket.on('setup', (userData) => {
+    socket.join(userData._id);
+    console.log(`User signed in and joined private room: ${userData._id}`);
+    socket.emit('connected');
+  });
+
+  socket.on('join chat', (roomId) => {
+    socket.join(roomId);
+    console.log(`User joined chat room: ${roomId}`);
+  });
+
+  socket.on('new message', (newMessageReceived) => {
+    const chat = newMessageReceived.chat;
+
+    if (!chat.users) return console.log('chat.users not defined');
+
+    chat.users.forEach((user) => {
+      if (user._id === newMessageReceived.sender._id) return;
+
+      socket.in(user._id).emit('message received', newMessageReceived);
+    });
+  });
+
+  socket.on('typing', (room) => socket.in(room).emit('typing'));
+  socket.on('stop typing', (room) => socket.in(room).emit('stop typing'));
+
+  socket.off('setup', () => {
+    console.log('User Disconnected ❌');
+    socket.leave(userData._id);
   });
 });
+
+// =========================================================================
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
